d672c2406ffde378589c16c8530a464870ad8bbe
WagnerAndrade87 <wagnermandrade@gmail.com>
2025-05-05 20:09:26 -0300

    Entrega do Projeto

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-/*# 2️⃣ Calculadora de partidas Rankeadas
-**O Que deve ser utilizado**
-
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
-
-## Objetivo:
-
-Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
-
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
-## Saída
-
-Ao final deve se exibir uma mensagem:
-"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
- 
-
-  
-Bons estudos 😉*/
-
-//let numberOfWins
-//let numberOfDefeatss
-
 let numberOfWins = ranked (50,10)
-let ranking = classificacao(numberOfWins)
+let ranking = classification(numberOfWins)
 
 console.log(ranking)
 
@@ -43,7 +9,7 @@ function ranked (wins, defeats){
     return rankedBalance
 }
 
-function classificacao (vitorias) {
+function classification (vitorias) {
     switch (true) {
         case vitorias >= 101:
             return `O Herói tem ${vitorias} vitórias de saldo está no nível Imortal`
